3f5caa3eb1c2ea5db9a9d07e421d515e814cfa33
tron_ubuntu2 <hanchong1996@hotmail.com>
2020-02-05 17:07:42 -0500

    vector working

--- src/node.py
+++ src/node.py
@@ -7,6 +7,7 @@ Node 1 handles numbers from 1 to 5
 import time
 import rospy
 import random
+import sys
 
 from std_msgs.msg import Int64
 from dist_num.msg import Feature # pylint: disable = import-error
@@ -37,7 +38,8 @@ class node0:
     # Input: a randomly generated vector and a given matrix
     # Output: index of the closest row vector in the given matrix
     def closest_vector(self, generated_vector, matrix):
-        min_dist = res_index = float('-inf')
+        min_dist = float('inf')
+        res_index = -sys.maxint - 1
         for r in range(len(matrix)):
             dist = self.dist_bet_vectors(generated_vector, matrix[r])
             if dist < min_dist:
@@ -49,7 +51,7 @@ class node0:
         data, from_id, to_id = feature.data, feature.header.frame_id, feature.to_id
         if to_id == self.node_id:
             self.total_count += 1
-            self.d[data] = self.d.get(data, 0) + 1
+            self.collection.append(list(data))
 
     def main(self):
         sub = rospy.Subscriber('/numbers', Feature, self.callback)
@@ -58,12 +60,19 @@ class node0:
         rate = rospy.Rate(self.publish_rate)
         feature = Feature()
 
+        # pausing for 1 second before start can ensure the first messages are not lost
+        # otherwise the first messages are lost
+        # number of lost messages = number of nodes initialized
+        time.sleep(1)
+
         for i in range(self.amount_generated):
             # generate random vector with set higher bound and lower bound
-            generated_vector = random.uniform(self.lower_bound, self.higher_bound)
+            generated_vector = [random.uniform(self.lower_bound, self.higher_bound), random.uniform(self.lower_bound, self.higher_bound)]
             belongs_to_index = self.closest_vector(generated_vector, self.matrix)
-
-            ############### continue here!!! ######################
+            print 'generated vector:'
+            print generated_vector
+            print 'belongs to index:'
+            print belongs_to_index
 
             # The integer generated is in the range for the node to process itself
             if belongs_to_index == self.node_id:
@@ -71,30 +80,30 @@ class node0:
                 self.collection.append(generated_vector)
             # otherwise broadcast it in a message through rostopic
             else:
-                feature.data = generated_int
+                feature.data = generated_vector
                 feature.header.frame_id = str(self.node_id)
-                feature.to_id = 2
+                feature.to_id = belongs_to_index
                 pub.publish(feature)
             rate.sleep()
         
         print "**********************************************"
-        print "NODE 1 FINAL RESULT: "
+        print rospy.get_name() + " FINAL RESULT: "
         print "Total numbers processed: " + str(self.total_count)
         print "(number: frequency)"
-        print self.d
+        print self.collection
         print "**********************************************"
 
         # Plot bar graph for distribution of frequencies
-        objects = [str(k) for k in self.d.keys()]
-        y_pos = np.arange(len(objects))
-        performance = [v for v in self.d.values()]
+        # objects = [str(k) for k in self.d.keys()]
+        # y_pos = np.arange(len(objects))
+        # performance = [v for v in self.d.values()]
 
-        plt.bar(y_pos, performance, align='center', alpha=0.5)
-        plt.xticks(y_pos, objects)
-        plt.ylabel('Frequency')
-        plt.title('Frequency of random numbers [Node 1]')
+        # plt.bar(y_pos, performance, align='center', alpha=0.5)
+        # plt.xticks(y_pos, objects)
+        # plt.ylabel('Frequency')
+        # plt.title('Frequency of random numbers [Node 1]')
 
-        plt.show()
+        # plt.show()
 
 if __name__ == '__main__':
     n = node0()
