abe57151f1f02c29ac74ea532be796817460aa7e
tron_ubuntu2 <hanchong1996@hotmail.com>
2020-02-20 14:14:09 -0500

    temp2

--- src/node.py
+++ src/node.py
@@ -9,6 +9,7 @@ import glob
 
 from std_msgs.msg import Int64
 from dist_num.msg import Feature # pylint: disable = import-error
+from scipy.spatial import distance_matrix
 
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
@@ -27,10 +28,20 @@ class Node:
         rospy.init_node('Node')
         self.features = []
         self.total_count = 0
-        # self.label_matrix = 
         self.node_id = rospy.get_param("/node_ids" + rospy.get_name())
         self.publish_rate = rospy.get_param('publish_rate')
         self.publish_queue_size = rospy.get_param('publish_queue_size')
+        self.feature_method = rospy.get_param('feature_extration_method')
+        if self.feature_method == 'SIFT':
+            self.features = np.random.rand(3, 128) * 255
+        elif self.feature_method == 'ORB':
+            # check if ORB feature value is from 0 to 32
+            self.features = np.random.rand(3, 32) * 32
+        elif self.feature_method == 'SURF':
+            # check if SURF feature value is from 0 to 32
+            self.features = np.random.rand(3, 64) * 32
+        else:
+            raise Exception('Feature extraction method not supported.')
 
     def callback(self, feature):
         data, data_length = np.array(feature.data), feature.data_length
@@ -56,7 +67,7 @@ class Node:
 
         #Initialization of Important Constants and Features data structure
         threshold = .75
-        method = 'SIFT' #Can use 'SIFT','ORB', or 'SURF'
+        self.feature_method = 'SIFT' #Can use 'SIFT','ORB', or 'SURF'
 
         #Choose file path for main images
         dirname = path.abspath(path.join(__file__, "../.."))
@@ -67,52 +78,30 @@ class Node:
 
         # Get raw image files based on node_id
         image_list = []             # features handled by this node
-        image_list_other = []       # features handled by other nodes
         for filepaths in [sorted(glob.glob(images_file_path % ext)) for ext in ["jpg", "gif", "png", "tga"]]:
             for filepath in filepaths:
                 image_num = int(filepath[57:-4])
                 if  image_num == 2 * self.node_id + 1 or image_num == 2 * self.node_id + 2:
                     image_list.append(cv2.imread(filepath))
-                else:
-                    image_list_other.append(cv2.imread(filepath))
 
-        ################### Extract features for images handled by other nodes ###################
-
-        # print '\nExtracting features for images handled by other nodes'
-
-        #Import Data into [dxn] numpy array
-        features = self.extract_features(features, method, image_list_other)
-        features.num_img = len(features.images)
-
-        #This is the length and dimension of the feature vector
-        features.size = features.data.shape
-        # print('Number and Size of Features')
-        # print(features.size)
-
-        '''
-            ki = 128/N*i
-        '''
-
-        # Flatten the features to 1d vectors
         data_flattened = features.data.flatten()
-        # print 'data_flattened shape'
-        # print data_flattened.shape
 
         # publish the flattened features
         feature.data = data_flattened
         feature.data_length = features.data_length
         feature.to_id = rospy.get_param('/to_ids' + rospy.get_name())
         pub.publish(feature)
-        print '\nmin = ' + str(features.data.min())
-        print '\nmax = ' + str(features.data.max())
-
-        ################### REPEAT for images handled by this node ###################
-
-        # print '\nExtracting features for images handled by this node'
 
         #Import Data into [dxn] numpy array
-        features = self.extract_features(features, method, image_list)
+        features = self.extract_features(features, image_list)
         features.num_img = len(features.images)
+
+        #Compute distance between each feature to all the feature labels
+
+        #Find the minimum distance and corresponding label
+
+        #For each feature that doesn't belong to this node's label,
+        #publish the feature to other corresponding node
 
         #This is the length and dimension of the feature vector
         features.size = features.data.shape
@@ -122,18 +111,18 @@ class Node:
         print '\nmax = ' + str(features.data.max())
 
     # Import car images and run Sift to get dataset
-    def extract_features(self, features, method, image_list):
+    def extract_features(self, features, image_list):
 
         # Initalize data structures
-        if method == 'SIFT':
+        if self.feature_method == 'SIFT':
             features.data = np.empty((1,128), dtype = np.float64)
             features.data_length = 128
             sift = cv2.xfeatures2d.SIFT_create(500, 3, 0.1, 5, 1.6)
-        if method == 'ORB':
+        if self.feature_method == 'ORB':
             features.data = np.empty((1,32), dtype = np.float64)
             features.data_length = 32
             orb = cv2.ORB_create()
-        if method == 'SURF':
+        if self.feature_method == 'SURF':
             features.data = np.empty((1,64), dtype = np.float64)
             features.data_length = 64
             surf = cv2.xfeatures2d.SURF_create(400)
@@ -150,11 +139,11 @@ class Node:
         # For each image, extract sift features and organize them into right structures
         for i in range(0, len(features.images)):
             gray= cv2.cvtColor(features.images[i], cv2.COLOR_BGR2GRAY)
-            if method == 'SURF':
+            if self.feature_method == 'SURF':
                 kp, des = surf.detectAndCompute(gray, None)
-            if method == 'SIFT':
+            if self.feature_method == 'SIFT':
                 kp, des = sift.detectAndCompute(gray, None)
-            if method == 'ORB':
+            if self.feature_method == 'ORB':
                 kp, des = orb.detectAndCompute(gray, None)
             keypts = [p.pt for p in kp]
             member = np.ones(len(keypts)) * i
